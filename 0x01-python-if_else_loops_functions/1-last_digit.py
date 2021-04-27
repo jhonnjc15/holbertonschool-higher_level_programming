c524cf41bbf1d246d5fdf59f695487e3cc3be169
jhonnjc15 <jhonatanjcca@gmail.com>
2021-04-27 18:58:20 +0000

    fixing the print error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+if number < 0:
+    last = (-1 * number) % 10
+else:
+    last =  number % 10
 if number < 0 :
     last = -last
 if last > 5:
