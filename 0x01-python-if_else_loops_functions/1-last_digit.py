32ebf31a95cf80f4347a24dd18de777cccbe4c41
jhonnjc15 <jhonatanjcca@gmail.com>
2021-04-27 20:40:47 +0000

    FIxing the pep8 error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ else:
 if number < 0:
     last = -last
 if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than\
-            5".format(number, last))
+    print("Last digit of {:d} is {:d} and is greater\
+ than 5".format(number, last))
 elif last < 6 and last != 0:
     print("Last digit of {:d} is {:d} and is less than\
-            6 and not 0".format(number, last))
+ 6 and not 0".format(number, last))
 elif last == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last))
