3409ba6a3263f81b16305137ec2488a25efdc2bf
jhonnjc15 <jhonatanjcca@gmail.com>
2021-04-27 18:33:40 +0000

    Fix the error in the comparation of numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,9 +4,9 @@ number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if number < 0 :
     last = -last
-if abs(last) > 5:
+if last > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(number, last))
-elif abs(last) < 6 and abs(last) > 0:
+elif last < 6 and last != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last))
-else:
+elif last == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last))
