5935eb4a4f19b6d4b15a6fa9f3c50a51394f0401
jhonnjc15 <jhonatanjcca@gmail.com>
2021-04-29 18:19:34 +0000

    Adding and using the __name__ varible

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv
-num_arg = len(arguments) - 1
-str_argumen = "argument"
-if num_arg != 1:
-    str_argumen = str_argumen + "s"
-if num_arg != 0:
-    last_character = ":"
-else:
-    last_character = "."
-print("{:d} {:s}{:s}".format(num_arg, str_argumen, last_character))
-for i in range(1, num_arg + 1):
-    print("{:d}: {:s}".format(i, arguments[i]))
+
+def prints_num_arg(): 
+    arguments = sys.argv
+    num_arg = len(arguments) - 1
+    str_argumen = "argument"
+    if num_arg != 1:
+        str_argumen = str_argumen + "s"
+    if num_arg != 0:
+        last_character = ":"
+    else:
+        last_character = "."
+    print("{:d} {:s}{:s}".format(num_arg, str_argumen, last_character))
+    for i in range(1, num_arg + 1):
+        print("{:d}: {:s}".format(i, arguments[i]))
+if __name__ == "__main__":
+    prints_num_arg()
