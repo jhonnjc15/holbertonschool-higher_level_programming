b8d82e495c0e1ec955d9a17daec1932332810617
jhonnjc15 <jhonatanjcca@gmail.com>
2021-04-29 18:13:50 +0000

    Program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+arguments = sys.argv
+num_arg = len(arguments) - 1
+str_argumen = "argument"
+if num_arg != 1:
+    str_argumen = str_argumen + "s"
+if num_arg != 0:
+    last_character = ":"
+else:
+    last_character = "."
+print("{:d} {:s}{:s}".format(num_arg, str_argumen, last_character))
+for i in range(1, num_arg + 1):
+    print("{:d}: {:s}".format(i, arguments[i]))
