cd0127744f4df95e86155f6e78b2a09cda08c5b0
jhonnjc15 <jhonatanjcca@gmail.com>
2021-05-04 19:37:47 +0000

    Changing the remove function to the delete funcion.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,5 @@ def delete_at(my_list=[], idx=0):
         if (idx < 0) or (idx > len(my_list) - 1):
             return my_list
         else:
-            drop_element = my_list[idx]
-            my_list.remove(drop_element)
+            del my_list[idx]
             return my_list
