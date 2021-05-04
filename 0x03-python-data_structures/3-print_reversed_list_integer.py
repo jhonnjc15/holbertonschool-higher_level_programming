139038f60bef862e28eb6d4f8bcddd88fbcdea27
jhonnjc15 <jhonatanjcca@gmail.com>
2021-05-04 18:10:43 +0000

    Adding the empty condition to the input list.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    """Condition to know if the list is empty"""
+    if my_list:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
