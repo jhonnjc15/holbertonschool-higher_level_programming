f51d74b37781e9c61be57bbec9fbb9e061dae703
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-02 05:30:01 +0000

    Write a class BaseGeometry, with Public instance method: area, Public instance method: integer_validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""7-base_geometry.py: MOdule that create a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+        Create a BaseGeometry class
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+            Area method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+            Checks if value is a integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
