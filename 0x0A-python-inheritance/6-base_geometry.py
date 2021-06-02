4ce051ee03e8dd258773fb65f6b1fe506ee2d756
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-02 05:27:44 +0000

    Write a class BaseGeometry, with Public instance method: def area(self):

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""5-base_geometry.py: MOdule that create a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+        Create a BaseGeometry class
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+            Area method
+        """
+        raise Exception("area() is not implemented")
