1a77578a9c403bf597b667ee8b11fa304c902d51
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-08 21:49:26 +0000

    Adding the public method update(), that assigns attributes.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,3 +43,32 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Update the instances with new arguments
+        Args:
+            args: tuple with the new arguments
+            kwargs: a dictionary with new arguments
+        """
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.size = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
