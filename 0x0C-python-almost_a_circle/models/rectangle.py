eddea68f837ace5bb8fa77fd16db0b3a376f0538
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-08 18:46:19 +0000

    Adding the __str__ method.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,11 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        """
+        Return string of rectangle instance's stats
+        """
+        return "[Rectangle] ({})"\
+               " {}/{} - {}/{}".format(self.id, self.__x, self.__y,
+                                       self.__width, self.__height)
