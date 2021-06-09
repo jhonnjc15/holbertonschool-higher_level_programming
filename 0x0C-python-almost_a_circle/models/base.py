89a182f13e2a663cd35cef6094ca0ed90f35ddc7
jhonnjc15 <jhonatanjcca@gmail.com>
2021-06-09 02:48:09 +0000

    Adding the class method create(), that returns an instance with all attributes already set.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,3 +58,17 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Return a new instance with all arguments
+        Args:
+            **dictionary : dictionary with all the arguments
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
