ea70fa7f21acd4a6f17fe9b40c63eb2ab1eed714
jhonnjc15 <jhonatanjcca@gmail.com>
2021-08-14 03:51:28 +0000

    Scripts that prints all City objects from the database hbtn_0e_14_usa and Python file similar to model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python file similar to model_state.py named
+model_city.py that contains the class definition of a City.
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Represents a city for a MySQL database.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
